3dd6fde766f93bfc7646a52f71cbe1d4a36d3426
Sefat Anam <sefatanam@gmail.com>
2020-04-10 13:33:38 +0600

    table added

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -5,7 +5,7 @@ import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.componen
 import { RouterModule } from '@angular/router';
 import { PostsComponent } from 'src/app/modules/posts/posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import {MatSidenavModule, MatDividerModule, MatCardModule} from '@angular/material';
+import {MatSidenavModule, MatDividerModule, MatCardModule, MatTableModule, MatPaginatorModule} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { DashboardService } from 'src/app/modules/dashboard.service';
 
@@ -24,7 +24,9 @@ import { DashboardService } from 'src/app/modules/dashboard.service';
     MatSidenavModule,
     MatDividerModule,
     MatCardModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [
     DashboardService
