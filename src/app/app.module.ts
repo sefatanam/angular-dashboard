333f058876168e54a529301a98c7f57ac81cc291
Sefat Anam <sefatanam@gmail.com>
2020-04-11 14:26:12 +0600

    ngrx install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
+import { StoreModule} from '@ngrx/store';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    
-  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    DefaultModule
-  
+    DefaultModule,
+    StoreModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
